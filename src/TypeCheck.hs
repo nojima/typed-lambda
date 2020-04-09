f54f3e37310b499c41bb1a990468b73b9fbf5813
Yusuke Nojima <nojima@ynojima.com>
2020-04-09 23:23:47 +0900

    Clean up

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -13,26 +13,7 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import           Control.Monad
 
-newtype TypeError = TypeError T.Text
-
-data TypingState = TypingState
-    { stateNextId :: !Int }
-    deriving (Eq, Show)
-
-type TypeChecker a = Except.ExceptT TypeError (State.State TypingState) a
-
-data TypeScheme = ForAll [Type.Variable] Type
-
--- 型環境
--- 変数名からその型へのマップ。
-type Env = Map.Map Identifier TypeScheme
-
--- 型制約
--- 等式 S = T の集合。ただし S と T は型。
-type Constraints = [Constraint]
-
-data Constraint =
-    CEqual Type Type SourcePos
+-------------------------------------------------------------------------------
 
 -- 型変数の集合
 type Variables = [Type.Variable]
@@ -44,6 +25,143 @@ newVariable = do
     let var = Identifier.fromText $ T.pack $ 't' : show nextId
     State.put $ state { stateNextId = nextId + 1 }
     return var
+
+-------------------------------------------------------------------------------
+
+data TypeScheme = ForAll [Type.Variable] Type
+
+-- 型を一般化して型スキームにする。
+generalize :: Env -> Type -> TypeScheme
+generalize env type_ =
+    case type_ of
+        Type.Function arg ret ->
+            let
+                ForAll argVars argType = generalize env arg
+                ForAll retVars retType = generalize env ret
+            in
+            ForAll (argVars <> retVars) (Type.Function argType retType)
+
+        Type.Var var | not (variableExists var env) ->
+            ForAll [var] type_
+
+        _ ->
+            ForAll [] type_
+
+-- 型スキームを具体化する。
+instantiate :: TypeScheme -> TypeChecker Type
+instantiate (ForAll vars type_) = do
+    sub <- forM vars $ \var -> do
+        newVar <- newVariable
+        return (var, Type.Var newVar)
+    return $ applySubstitution sub type_
+
+-------------------------------------------------------------------------------
+
+-- 型環境
+-- 変数名からその型へのマップ。
+type Env = Map.Map Identifier TypeScheme
+
+-- 型環境で指定された型変数が言及されているなら True を返す。
+variableExists :: Type.Variable -> Env -> Bool
+variableExists var =
+    any $ \(ForAll _ type_) -> f type_
+  where
+    f type_ =
+        case type_ of
+            Type.Function arg ret ->
+                f arg || f ret
+            Type.Var v | v == var ->
+                True
+            _ ->
+                False
+
+-------------------------------------------------------------------------------
+
+-- 型代入
+type Substitution = [(Type.Variable, Type)]
+
+substituteConstraints :: Type.Variable -> Type -> Constraints -> Constraints
+substituteConstraints var type_ =
+    map $ \(CEqual lhs rhs pos) ->
+        CEqual (substituteType var type_ lhs) (substituteType var type_ rhs) pos
+
+substituteType :: Type.Variable -> Type -> Type -> Type
+substituteType var type_ target =
+    case target of
+        Type.Function arg ret ->
+            Type.Function (substituteType var type_ arg) (substituteType var type_ ret)
+        Type.Var v | v == var ->
+            type_
+        _ ->
+            target
+
+applySubstitution :: Substitution -> Type -> Type
+applySubstitution sub =
+    apply (Map.fromList sub)
+  where
+    apply :: Map.Map Type.Variable Type -> Type -> Type
+    apply sub_ type_ =
+        case type_ of
+            Type.Function arg ret ->
+                Type.Function (apply sub_ arg) (apply sub_ ret)
+            Type.Var v ->
+                case Map.lookup v sub_ of
+                    Just t ->
+                        apply sub_ t
+                    Nothing ->
+                        type_
+            _ ->
+                type_
+
+-------------------------------------------------------------------------------
+
+-- 型制約
+-- 等式 S = T の集合。ただし S と T は型。
+type Constraints = [Constraint]
+
+data Constraint =
+    CEqual Type Type SourcePos
+
+-- 型制約を充足するような最も一般的な型代入を返す。
+unify :: Constraints -> Either TypeError Substitution
+unify [] = return []
+unify (CEqual type1 type2 pos : cs) =
+    if type1 == type2 then
+        unify cs
+    else
+        case (type1, type2) of
+            (Type.Var var1, _) ->
+                (:) <$> pure (var1, type2) <*> unify (substituteConstraints var1 type2 cs)
+
+            (_, Type.Var var2) ->
+                (:) <$> pure (var2, type1) <*> unify (substituteConstraints var2 type1 cs)
+
+            (Type.Function arg1 ret1, Type.Function arg2 ret2) ->
+                unify (CEqual arg1 arg2 pos : CEqual ret1 ret2 pos : cs)
+
+            _ ->
+                let
+                    errorMessage =
+                        T.pack (Term.sourcePosPretty pos)
+                        <> ": type mismatch\n"
+                        <> "  expected: "
+                        <> Type.pretty type2
+                        <> "\n"
+                        <> "  actual: "
+                        <> Type.pretty type1
+                        <> "\n"
+                in
+                Left $ TypeError errorMessage
+
+-------------------------------------------------------------------------------
+
+type TypeChecker a = Except.ExceptT TypeError (State.State TypingState) a
+
+newtype TypeError = TypeError T.Text
+
+newtype TypingState = TypingState
+    { stateNextId :: Int }
+    deriving (Eq, Show)
 
 mustBe :: Type -> SourcePos -> Env -> Term -> TypeChecker (Constraints, Variables)
 mustBe type_ pos env term = do
@@ -164,109 +282,7 @@ constrain env term =
             (bodyType, bodyConstraints, bodyVars) <- constrain bodyEnv body
             return (bodyType, exprConstraints <> bodyConstraints, exprVars <> bodyVars)
 
--- 型スキームを具体化する
-instantiate :: TypeScheme -> TypeChecker Type
-instantiate (ForAll vars type_) = do
-    sub <- forM vars $ \var -> do
-        newVar <- newVariable
-        return (var, Type.Var newVar)
-    return $ applySubstitution sub type_
-
-generalize :: Env -> Type -> TypeScheme
-generalize env type_ =
-    case type_ of
-        Type.Function arg ret ->
-            let
-                ForAll argVars argType = generalize env arg
-                ForAll retVars retType = generalize env ret
-            in
-            ForAll (argVars <> retVars) (Type.Function argType retType)
-
-        Type.Var var | not (variableExists var env) ->
-            ForAll [var] type_
-
-        _ ->
-            ForAll [] type_
-
-variableExists :: Type.Variable -> Env -> Bool
-variableExists var =
-    any $ \(ForAll _ type_) -> f type_
-  where
-    f type_ =
-        case type_ of
-            Type.Function arg ret ->
-                f arg || f ret
-            Type.Var v | v == var ->
-                True
-            _ ->
-                False
-
--- 型代入
-type Substitution = [(Type.Variable, Type)]
-
--- 型制約を充足するような最も一般的な型代入を返す。
-unify :: Constraints -> Either TypeError Substitution
-unify [] = return []
-unify (CEqual type1 type2 pos : cs) =
-    if type1 == type2 then
-        unify cs
-    else
-        case (type1, type2) of
-            (Type.Var var1, _) ->
-                (:) <$> pure (var1, type2) <*> unify (substituteConstraints var1 type2 cs)
-
-            (_, Type.Var var2) ->
-                (:) <$> pure (var2, type1) <*> unify (substituteConstraints var2 type1 cs)
-
-            (Type.Function arg1 ret1, Type.Function arg2 ret2) ->
-                unify (CEqual arg1 arg2 pos : CEqual ret1 ret2 pos : cs)
-
-            _ ->
-                let
-                    errorMessage =
-                        T.pack (Term.sourcePosPretty pos)
-                        <> ": type mismatch\n"
-                        <> "  expected: "
-                        <> Type.pretty type2
-                        <> "\n"
-                        <> "  actual: "
-                        <> Type.pretty type1
-                        <> "\n"
-                in
-                Left $ TypeError errorMessage
-
-substituteConstraints :: Type.Variable -> Type -> Constraints -> Constraints
-substituteConstraints var type_ =
-    map $ \(CEqual lhs rhs pos) ->
-        CEqual (substituteType var type_ lhs) (substituteType var type_ rhs) pos
-
-substituteType :: Type.Variable -> Type -> Type -> Type
-substituteType var type_ target =
-    case target of
-        Type.Function arg ret ->
-            Type.Function (substituteType var type_ arg) (substituteType var type_ ret)
-        Type.Var v | v == var ->
-            type_
-        _ ->
-            target
-
-applySubstitution :: Substitution -> Type -> Type
-applySubstitution sub =
-    apply (Map.fromList sub)
-  where
-    apply :: Map.Map Type.Variable Type -> Type -> Type
-    apply sub_ type_ =
-        case type_ of
-            Type.Function arg ret ->
-                Type.Function (apply sub_ arg) (apply sub_ ret)
-            Type.Var v ->
-                case Map.lookup v sub_ of
-                    Just t ->
-                        apply sub_ t
-                    Nothing ->
-                        type_
-            _ ->
-                type_
+-------------------------------------------------------------------------------
 
 typeCheck :: Term -> Either TypeError Type
 typeCheck term = do
