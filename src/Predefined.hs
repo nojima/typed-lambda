1b42133cc0c9cbc8daf4d18857ed07ef3cc7b2f6
Yusuke Nojima <nojima@ynojima.com>
2020-04-10 21:18:55 +0900

    Add list functions

--- src/Predefined.hs
+++ src/Predefined.hs
@@ -21,6 +21,21 @@ functions =
         (ForAll ["a"]
             (Type.Function (Type.List (Type.Var "a")) (Type.Var "a")))
         pdHead
+    , Function
+        "tail"
+        (ForAll ["a"]
+            (Type.Function (Type.List (Type.Var "a")) (Type.List (Type.Var "a"))))
+        pdTail
+    , Function
+        "null"
+        (ForAll ["a"]
+            (Type.Function (Type.List (Type.Var "a")) Type.Bool))
+        pdNull
+    , Function
+        "cons"
+        (ForAll ["a"]
+            (Type.Function (Type.Var "a") (Type.Function (Type.List (Type.Var "a")) (Type.List (Type.Var "a")))))
+        pdCons1
     ]
 
 pdHead :: Value -> Either RuntimeError Value
@@ -33,3 +48,34 @@ pdHead value =
                 Right $ head elements
         _ ->
             Left $ RuntimeError "head: the given value is not a list"
+
+pdTail :: Value -> Either RuntimeError Value
+pdTail value =
+    case value of
+        Value.List elements ->
+            if null elements then
+                Left $ RuntimeError "tail: the given list is empty"
+            else
+                Right $ Value.List (tail elements)
+        _ ->
+            Left $ RuntimeError "tail: the given value is not a list"
+
+pdNull :: Value -> Either RuntimeError Value
+pdNull value =
+    case value of
+        Value.List elements ->
+            Right $ Value.Bool (null elements)
+        _ ->
+            Left $ RuntimeError "null: the given value is not a list"
+
+pdCons1 :: Value -> Either RuntimeError Value
+pdCons1 value =
+    Right $ Value.NativeFunction "cons" (pdCons2 value)
+
+pdCons2 :: Value -> Value -> Either RuntimeError Value
+pdCons2 value1 value2 =
+    case value2 of
+        Value.List elements ->
+            Right $ Value.List (value1:elements)
+        _ ->
+            Left $ RuntimeError "cons: the 2nd argument is not a list"
